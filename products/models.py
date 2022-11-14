645754b58643b5b81b5879c83953c3e385fd3fa4
kyy <insider_2012@mail.ru>
2022-11-14 16:00:45 +0300

    some text

--- products/models.py
+++ products/models.py
@@ -1,16 +1,24 @@
 from django import forms
 from django.db import models
+from django.utils.safestring import mark_safe
 from modelcluster.contrib.taggit import ClusterTaggableManager
-from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.fields import ParentalKey
 from taggit.models import TaggedItemBase
 from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.admin.panels import InlinePanel, MultiFieldPanel
+from wagtail.admin.panels import InlinePanel
 from wagtail.fields import RichTextField
 from wagtail.models import Page, Orderable
 from wagtail.search import index
 from wagtail.snippets.models import register_snippet
-from wagtail.signals import page_published
 
+
+
+
+#constants
+max_product_image_numbers = 5
+links = {'new_product_category':
+             '<a target="_blank" href="/admin/snippets/products/productcategory/add/"> создать категорию.</a>',
+}
 
 
 class ProductIndexPage(Page):
@@ -43,12 +51,11 @@ class ProductIndexPage(Page):
         context['productpages'] = productpages
         return context
 
-
     intro = RichTextField(blank=True)
-
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]
+
 
 class ProductPageTag(TaggedItemBase):
     content_object = ParentalKey(
@@ -61,6 +68,9 @@ class ProductPageTag(TaggedItemBase):
 class ProductPage(Page):
     subpage_types = []
     parent_page_types = ['products.ProductIndexPage']
+    page_description = "Пополните каталог используя эту страницу"
+
+    Page._meta.get_field("title").help_text = 'Данное имя может отображаться как заголовок.'
 
     # даем доступ к изображениям на странице
     def main_image(self):
@@ -72,8 +82,15 @@ class ProductPage(Page):
 
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
-    tags = ClusterTaggableManager(through=ProductPageTag, blank=True)
-    categories = models.ForeignKey('products.ProductCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    tags = ClusterTaggableManager(through=ProductPageTag,
+                                  blank=True
+                                  )
+    categories = models.ForeignKey('products.ProductCategory',
+                                   null=True,
+                                   blank=True,
+                                   on_delete=models.SET_NULL,
+                                   related_name='name_category'
+                                   )
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
@@ -81,26 +98,47 @@ class ProductPage(Page):
     ]
 
     content_panels = Page.content_panels + [
-        MultiFieldPanel([
-            FieldPanel('tags'),
-            FieldPanel('categories', widget=forms.RadioSelect),
-        ], heading="Информация о компоненте"),
-        FieldPanel('intro'),
-        FieldPanel('body'),
-        InlinePanel('gallery_images', label="Gallery images"),
+        FieldPanel('tags',
+                   help_text='Краткая характеристика в виде тегов',
+                   ),
+        FieldPanel('categories',
+                   widget=forms.RadioSelect,
+                   heading='Категория',
+                   help_text= mark_safe('Выберите категорию изделия. Если отсутствует подходящая, вы '
+                                        'можете оставить поле пустым или'+links['new_product_category']+
+                                        '<br>Располодение категорий: Фрагменты/Категории каталога продукции/...')
+                   ),
+        FieldPanel('intro',
+                   heading = 'Полное наименование изделия'
+                   ),
+        FieldPanel('body',
+                   heading='Полное описание',
+                   help_text ='Описание отображается только на индивидуальной странице.',
+                   ),
+        InlinePanel('gallery_images',
+                    heading='Галерея',
+                    label="Изображение",
+                    help_text='Первое изображение также будет отображаться в каталоге.'
+                              f' Максимальное уоличество изображений - {max_product_image_numbers}.',
+                    max_num=max_product_image_numbers
+                    ),
     ]
 
 
 class BlogPageGalleryImage(Orderable):
-    page = ParentalKey(ProductPage, on_delete=models.CASCADE, related_name='gallery_images')
-    image = models.ForeignKey(
-        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
-    )
-    caption = models.CharField(blank=True, max_length=250)
-
+    page = ParentalKey(ProductPage, on_delete=models.CASCADE,
+                       related_name='gallery_images',
+                       )
+    image = models.ForeignKey('wagtailimages.Image',
+                              on_delete=models.CASCADE,
+                              related_name='+',
+                              )
+    caption = models.CharField(blank=True,
+                               max_length=250,
+                               )
     panels = [
-        FieldPanel('image'),
-        FieldPanel('caption'),
+        FieldPanel('image', heading='Превью'),
+        FieldPanel('caption', heading='Подпись'),
     ]
 
 
@@ -122,22 +160,32 @@ class ProductTagIndexPage(Page):
 
 @register_snippet
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     icon = models.ForeignKey(
         'wagtailimages.Image', null=True, blank=True,
         on_delete=models.SET_NULL, related_name='+'
     )
 
     panels = [
-        FieldPanel('name'),
-        FieldPanel('icon'),
+        FieldPanel('name',
+                   help_text='Данная категория будет доступна при пополнении каталога продукции',
+                   heading='Имя категории'
+                   ),
+        FieldPanel('icon',
+                   help_text='Выберите визуальный образ характеризующий категорию',
+                   heading='Иконка категории'
+                   ),
     ]
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = 'product categories'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории каталога продукции'
+
+
+
 
 
 
