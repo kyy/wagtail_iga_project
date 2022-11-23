6800642873d3138a25dddbd53a120cf194b6aafd
kyy <insider_2012@mail.ru>
2022-11-23 15:48:02 +0300

    css editing

--- products/templatetags/tags.py
+++ products/templatetags/tags.py
@@ -7,13 +7,13 @@ register = template.Library()
 @register.inclusion_tag('products/tags/live_categories.html', takes_context=True)
 def live_categories_menu(context):
     return {
-        'categories': live_categories,
+        'live_categories': live_categories,
         'request': context['request'],
     }
 
 @register.inclusion_tag('products/tags/live_categories_burger_menu.html', takes_context=True)
 def live_categories_burger_menu(context):
     return {
-        'categories': live_categories,
+        'live_categories': live_categories,
         'request': context['request'],
     }
